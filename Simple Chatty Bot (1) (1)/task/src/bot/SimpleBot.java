994731edc254539a36a43920ca17530871ae65e9
annascott <workwithpurpose@yahoo.com>
2020-08-21 21:37:18 -0600

    Stage 3

--- Simple Chatty Bot (1) (1)/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot (1) (1)/task/src/bot/SimpleBot.java
@@ -1,11 +1,20 @@
 package bot;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class SimpleBot {
 
     private String name;
     private int birthYear;
+    BufferedReader reader;
+    Scanner scn;
+
+    public SimpleBot() {
+        reader =new BufferedReader(new InputStreamReader(System.in));
+        scn = new Scanner(System.in);
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -23,15 +32,26 @@ public class SimpleBot {
         return birthYear;
     }
 
-    public void meetAndGreet() {
-        Scanner scn = new Scanner(System.in);
+    public void meetAndGreet() throws IOException {
+
         System.out.printf("Hello! My name is %s.", name);
         System.out.printf("\nI was created in %d.", birthYear);
         System.out.print("\nPlease, remind me your name.");
 
-        System.out.printf("\nWhat a great name you have, %s", scn.nextLine());
+        System.out.printf("\nWhat a great name you have, %s%n", reader.readLine());
 
     }
+
+    public void calculateAge() {
+        System.out.print("Let me guess your age.\n");
+        System.out.print("Enter remainders of dividing your age by 3, 5 and 7.");
+        int age = (scn.nextInt() * 70 + scn.nextInt() * 21 + scn.nextInt() * 15) % 105;
+        System.out.printf("\nYour age is %d; that's a good time to start programming!", age );
+
+
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -39,13 +59,18 @@ public class SimpleBot {
         SimpleBot bot = new SimpleBot();
         bot.setName("Aid");
         bot.setBirthYear(2020);
-        bot.meetAndGreet();
+        try {
+            bot.meetAndGreet();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        bot.calculateAge();
     }
+
 }
 
-
 /*import java.util.Scanner;
-
 public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -54,8 +79,14 @@ public class SimpleBot {
         System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
+        String name = scanner.nextLine();
 
-        System.out.println("What a great name you have, {yourName}!");
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+
+        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
     }
 }*/
